c07ba35e601d323e563cae98a326a25bc1a7a9f2
Sublio <sublio1260@gmail.com>
2016-04-26 21:39:37 +0600

    Initial Commit

---
+++ PopoversTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PopoversTest
+//
+//  Created by sublio on 26/04/16.
+//  Copyright (c) 2016 sublio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PopoversTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PopoversTest
+//
+//  Created by sublio on 26/04/16.
+//  Copyright (c) 2016 sublio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
