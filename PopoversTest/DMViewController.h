324d0d06d282644403a04301791b4956c1e432cc
Sublio <sublio1260@gmail.com>
2016-04-30 22:04:30 +0600

    Structure has rewritten

---
+++ PopoversTest/DMViewController.h
@@ -0,0 +1,17 @@
+//
+//  DMViewController.h
+//  PopoversTest
+//
+//  Created by sublio on 30/04/16.
+//  Copyright (c) 2016 sublio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DMViewController : UIViewController
+
+
+- (IBAction) actionAdd:(UIBarButtonItem*)sender;
+- (IBAction) actionPressMe:(UIButton*)sender;
+
+@end
